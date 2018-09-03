7052555bfd7a39a6a38c38e7b6ed0553753885c3
Christian Chiconato <cchiconato@gmail.com>
2018-09-03 00:03:05 -0300

    busca para prodtuos

---
+++ src/main/java/com/nelioalves/cursomc/resources/utils/URL.java
@@ -0,0 +1,22 @@
+package com.nelioalves.cursomc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class URL {
+	
+	public static List<Integer> decodeIntList(String ids) {
+		return Arrays.asList(ids.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+	}
+	
+	public static String decodeParem(String param) {
+		try {
+			return URLDecoder.decode(param, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+}
