1d8cc5e291059d3d3624b2bec042aba3ec46ed2d
Christian Chiconato <cchiconato@gmail.com>
2018-09-02 22:14:05 -0300

    Validacao de CPF e CNPJ

--- src/main/java/com/nelioalves/cursomc/dto/ClienteInsertDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteInsertDTO.java
@@ -2,22 +2,46 @@ package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+import com.nelioalves.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteInsertDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entro 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty
+	@Email(message="Email enválido")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
+	
 	private String telefone3;
 	
 	private Integer cidadeId;
