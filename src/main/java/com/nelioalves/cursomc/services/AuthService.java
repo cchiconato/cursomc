d900ffa73755b245d9cdf62e76292ac11bdf48fc
Christian Chiconato <cchiconato@gmail.com>
2018-09-05 00:32:39 -0300

    Esqueci a senha implementado

---
+++ src/main/java/com/nelioalves/cursomc/services/AuthService.java
@@ -0,0 +1,56 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.exceptions.ObjectNotFoundException;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
+
+@Service
+public class AuthService {
+
+	@Autowired
+	private ClienteRepository clienteRepo;
+	@Autowired
+	private BCryptPasswordEncoder pe;
+	@Autowired
+	private EmailService emailService;
+	
+	private Random random = new Random();
+	
+	public void sendNewPassword(String email) {
+		Cliente cliente = clienteRepo.findByEmail(email);
+		if(cliente == null) {
+			throw new ObjectNotFoundException("Email não encontrado");
+		}
+		
+		String newPass = newPassword();
+		cliente.setSenha(pe.encode(newPass));
+		
+		clienteRepo.save(cliente);
+		emailService.sendNewPasswordEmail(cliente, newPass);
+	}
+
+	private String newPassword() {
+		char[] vet = new char[10];
+		for (int i = 0; i < vet.length; i++) {
+			vet[i] = randomChar();
+		}
+		return new String(vet);
+	}
+
+	private char randomChar() {
+		int opt = random.nextInt(3);
+		if(opt == 0 ) {
+			return (char) (random.nextInt(10) + 48);
+		} else if (opt == 1) {
+			return (char) (random.nextInt(26) + 65);
+		} else {
+			return (char) (random.nextInt(26) + 97);
+		}
+	}
+}
